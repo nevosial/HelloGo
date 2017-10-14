b8edac7cc24574cb07297c2abb67f61a9398d84b
Neville Mascarenhas <nevosial@gmail.com>
2017-10-14 06:15:49 -0700

    Update hello.go

--- hello.go
+++ hello.go
@@ -6,8 +6,10 @@ import(
   "fmt"
   "math/rand"
   "time"
+  "math"
 )
 func main(){
   fmt.Println("Hello world, the time is: ", time.Now())
   fmt.Println("My favorite number is", rand.Intn(10))
+  fmt.Println(math.Pi)
   }
