4dc41497b6206fd2f046946182254a5efe29cc65
nevosial <nevosial@gmail.com>
2017-12-19 08:06:47 -0800

    Stacking defers

--- defer.go
+++ defer.go
@@ -8,4 +8,14 @@ func main(){
 
   fmt.Println("World")
 
+  fmt.Println("Stacking defers")
+
+  fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
+
 }
