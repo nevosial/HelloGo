ad2a7d3ae5bf640829612eaa0b931dd50db454c0
nevosial <nevosial@gmail.com>
2017-12-19 08:04:23 -0800

    Defer a function

---
+++ defer.go
@@ -0,0 +1,11 @@
+//A defer statement defers the execution of a function until the surrounding function returns.
+package main
+
+import "fmt"
+
+func main(){
+  defer fmt.Println("Hello")
+
+  fmt.Println("World")
+
+}
