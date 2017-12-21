84cd393b0b9bb9a2c767096a8c9b4b09aa6c4706
Neville Mascarenhas <nevosial@gmail.com>
2017-12-20 18:32:44 -0800

    structs in golang

--- structs.go
+++ structs.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+type Vertex struct {
+	X int
+	Y int
+}
+
 type Details struct {
 	name string
 	age int
@@ -12,8 +17,30 @@ type empDB struct {
 	age int
 }
 
-
 func main() {
-	fmt.Println(Details{"nev", 21})
+	v := Vertex{1, 2}
+	v.X = 4
+	fmt.Println(v.X)
+	
+	
+	//fmt.Println(Details{"nev", 21})
+	
+	//store records in the struct.
+	v2 := empDB{"Max", 2331}	
+	
+	//Printing values.
+	fmt.Println(v2)
+	
+	//Using dot
+	fmt.Println(v2.name)
+	fmt.Println(v2.age)
+	
 }
+
+
+
+
+
+
+
 
